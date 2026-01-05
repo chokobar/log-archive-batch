70005c40302f447250980f9b005863bb2369d2df
chokobar89 <chokobar89@naver.com>
2026-01-05 20:01:42 +0900

    Initial commit

---
+++ src/main/java/com/chokobar/batch/logarchive/LogArchiveBatchApplication.java
@@ -0,0 +1,13 @@
+package com.chokobar.batch.logarchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogArchiveBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogArchiveBatchApplication.class, args);
+	}
+
+}
