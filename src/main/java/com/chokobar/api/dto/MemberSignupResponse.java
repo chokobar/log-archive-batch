ed24f7dca21d0001c1be2f65b53772cb0d0bad8c
chokobar89 <chokobar89@naver.com>
2026-04-02 21:46:04 +0900

    Refactoring

--- src/main/java/com/chokobar/api/dto/MemberSignupResponse.java
+++ src/main/java/com/chokobar/api/dto/MemberSignupResponse.java
@@ -1,20 +1,16 @@
 package com.chokobar.api.dto;
 
 import com.chokobar.api.entity.Member;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class MemberSignupResponse {
 
     private final Long memberId;
     private final String loginId;
     private final String name;
-
-    public MemberSignupResponse(Long memberId, String loginId, String name) {
-        this.memberId = memberId;
-        this.loginId = loginId;
-        this.name = name;
-    }
 
     public static MemberSignupResponse from(Member member) {
         return new MemberSignupResponse(member.getId(), member.getLoginId(), member.getName());
