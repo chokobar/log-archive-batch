3617b2d38fc12a23be4ebd9d3139c33e573552f9
chokobar89 <chokobar89@naver.com>
2026-04-02 21:44:22 +0900

    수정

--- src/main/java/com/chokobar/api/entity/ApiCallLog.java
+++ src/main/java/com/chokobar/api/entity/ApiCallLog.java
@@ -1,13 +1,11 @@
 package com.chokobar.api.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
+@Setter
 @Getter
 @Entity
 @Table(name = "api_call_log",
